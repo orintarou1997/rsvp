d5c96cd8a92394681c21c4b31b79e12c116cf98c
Kuma C <13heartache.kc@gmail.com>
2023-11-08 18:04:34 -0500

    link update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <div>
           <img className="flyer" src={flyer}/>
           <h1>
-            <a className="link" href="https://secure.inspirave.com/id=36#toregister">RSVP</a>
+            <a className="link" href="https://secure.inspirave.com/id=15407#toregister">RSVP</a>
           </h1>        
         </div>
       </header>
