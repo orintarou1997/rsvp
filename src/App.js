4e9c32d61e069a4cd807a3db55ed619dd162503e
Kuma C <13heartache.kc@gmail.com>
2023-11-08 04:36:15 -0500

    font

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet"/>
       <header className="App-header">
         <div>
           <img className="flyer" src={flyer}/>
-
           <h1>
             <a className="link" href="https://secure.inspirave.com/">RSVP</a>
           </h1>        
