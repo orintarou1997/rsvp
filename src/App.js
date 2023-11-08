10bb2f39a0009e26b0de32aabe15176ade661b7b
Kuma C <13heartache.kc@gmail.com>
2023-11-08 16:30:33 -0500

    font change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
       <header className="App-header">
         <div>
           <img className="flyer" src={flyer}/>
